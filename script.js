05623af5e38bf35c9cfcc749fabc78ccf6ebd4ba
crashdev226 <midashand44@gmail.com>
2023-09-23 23:29:03 +0530

    make more ux and add validation in start page and add start button style

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-
 document.getElementById("startButton").onclick = function () {
-    localStorage.setItem("name", document.getElementById("nameInput").value);
-    window.location.href = "/chat.html";
-  };
+  //validation
+  if (document.getElementById("nameInput").value === "") {
+    alert("Please enter a name");
+    return;
+  }
+  localStorage.setItem("name", document.getElementById("nameInput").value);
+  window.location.href = "/chat.html";
+};
